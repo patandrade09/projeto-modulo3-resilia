22be66ec793ed23e08d0bdae0c72163bb8752c5a
patricia andrade <patricia.andrade094@gmail.com>
2021-10-24 22:22:05 -0300

    ajuste na pergunta 4

--- Queries/História Geral da F1/Resposta 4.sql
+++ Queries/História Geral da F1/Resposta 4.sql
@@ -1,10 +1,11 @@
--- resposta 2
+/* Quais as 5 construtoras que obtiveram mais vitórias na história da F1? */
 SELECT 
-    *
+    A.NAME AS CONSTRUTORA, SUM(WINS)
 FROM
-    races
-WHERE
-    name = 'Brazilian Grand Prix'
-ORDER BY year ASC
-LIMIT 1;
+    constructors A
+        INNER JOIN
+    ConstructorStandings B ON A.constructorId = B.constructorId
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5
 
